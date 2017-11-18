64d72b25c339ad2769b61864084ce0abd73ecec4
Anessa Arnold <anessaarnold@icloud.com>
2017-11-17 16:57:50 -0800

    updated passport.js

--- config/passport.js
+++ config/passport.js
@@ -4,9 +4,9 @@ var findOrCreate = require('mongoose-find-or-create');
 var User = require('./../models/User');
 
 passport.use(new GitHubStrategy({
-    clientID: 'cd51577bbc5831a29677',
-    clientSecret: 'bdb0ff1556beb9bfc9a716b9e1c0a70767a8c740',
-    callbackURL: "http://localhost:3000/auth/github/callback"
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log("in")
