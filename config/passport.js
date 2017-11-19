6253b7c14f12fd62ff2177994179a9080431d5a1
Patrick Finazzo <pfinazzoproduction@gmail.com>
2017-11-18 22:12:23 -0800

    got google passport working

--- config/passport.js
+++ config/passport.js
@@ -1,43 +1,48 @@
 var passport = require('passport');
-var GitHubStrategy = require('passport-github').Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 var User = require('../models/User')
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.GITHUB_CALLBACK
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOne({ 'githubId': profile.id }, function(err, student) {
-      if (err) return cb(err);
-      if (student) {
-        return cb(null, student);
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK
+},
+function(accessToken, refreshToken, profile, cb) {
+  // a user has logged in with OAuth...
+  User.findOne({ 'googleId': profile.id }, function(err, user) {
+    if (err) return cb(err);
+    if (user) {
+      if (!user.avatar) {
+        user.avatar = profile.photos[0].value;
+        user.save(function(err) {
+          return cb(null, user);
+        });
       } else {
-        // we have a new student via OAuth!
-        console.log(profile)
-        var newUser = new User({
-          fullName: profile.username,
-          githubId: profile.id
-        });
-        console.log(newUser)
-        newUser.save(function(err) {
-          if (err) return cb(err);
-          return cb(null, newUser);
-        });
+        return cb(null, user);
       }
-    });
-  }
+    } else {
+      // we have a new user via OAuth!
+      var newUser = new User({
+        name: profile.displayName,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value,
+        googleId: profile.id
+      });
+      newUser.save(function(err) {
+        if (err) return cb(err);
+        return cb(null, newUser);
+      });
+    }
+  });
+}
 ));
 
+passport.serializeUser(function(user, done) {
+done(null, user.id);
+});
 
- passport.serializeUser(function(student, done) {
-     done(null, student.id);
- });
-
-
- passport.deserializeUser(function(id, done) {
-   User.findById(id, function(err, student) {
-     done(err, student);
-   });
- });
-
+passport.deserializeUser(function(id, done) {
+User.findById(id, function(err, user) {
+  done(err, user);
+});
+});
--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,30 @@ router.get('/', function(req, res) {
   res.render('index', {user: req.user});
 });
 
-router.get('/auth/github/',
-  passport.authenticate('github'));
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/auth/google');
+}
 
- router.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('/');
-  });
 
 
 
