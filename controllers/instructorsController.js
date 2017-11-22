70eced6a5fe47fc2f57d817e392677234ac14b76
Patrick Finazzo <pfinazzoproduction@gmail.com>
2017-11-21 18:19:07 -0800

    partials n stuff

--- controllers/instructorsController.js
+++ controllers/instructorsController.js
@@ -1,7 +1,7 @@
 var Instructor = require('./../models/User');
 
 function index(req, res) {
-  res.render('./instructors/index');
+  res.render('./instructors/index', {currentUser});
 }
 
 module.exports = {
