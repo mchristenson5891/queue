da8a6673cb2ce00abe2fc262895115a646eacdba
Anessa Arnold <anessaarnold@icloud.com>
2017-11-26 17:06:53 -0800

    updating branch

--- seed.js
+++ seed.js
@@ -14,14 +14,8 @@ seed.then(() => {
     return User.create([
         { fullName: 'Jim', instructor: true },
         { fullName: 'Jon', instructor: true },
-        
-        { githubUserName: 'Bobby', instructor: false, quizzes:[ "5a15f87ad0c2e1f48901393d", "5a14ea1f54e047e25859d8e0", "5a15f89bd0c2e1f48901393f" ] },
-        { githubUserName: 'Time', instructor: false},
-        { githubUserName: 'Dani', instructor: false, cohort: "45"},
-        { githubUserName: 'Billy', instructor: false, cohort: "53"},
         { githubUserName: 'anessaa', instructor: false, cohort: "53", quizzes:["5a15f89bd0c2e1f48901393f"]},
-        { githubUserName: 'prancy', instructor: false, cohort: "45", quizzes:["5a15f89bd0c2e1f48901393f"]},
-        { githubUserName: 'pfinazzo', instructor: true, cohort: "45"}
+       
     ])
 })
 
